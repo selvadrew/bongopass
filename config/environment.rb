aa05674f620917244eee13d23ee51bfd1ce45195
selvadrew <andrew.selvadurai6@gmail.com>
2017-09-04 22:23:09 -0400

    mailer error

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+config.action_mailer.default_url_options = { :host => 'https://frozen-taiga-19769.herokuapp.com' }
