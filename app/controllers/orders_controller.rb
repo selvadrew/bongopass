8813841424bb83c2f0693e815047d2bca3fb2d9c
selvadrew <andrew.selvadurai6@gmail.com>
2017-09-07 18:39:56 -0400

    before adding referral system, partially fixed sold out tickets

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -72,32 +72,12 @@ class OrdersController < ApplicationController
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
-  end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,17 @@
 class Order < ApplicationRecord
-	validates :address, :city, :province, presence: true 
-
+	
 	belongs_to :ticket 
 
 	belongs_to :buyer, class_name: "User"
 	belongs_to :seller, class_name: "User"
+
+	validates :address, :city, :province, presence: true 
+	validate :quantity_available 
+
+	def quantity_available
+		if ticket.ticket_quantity == 0 
+			self.errors.add(:base, "This ticket has sold out")
+		end
+	end
 	
 end
