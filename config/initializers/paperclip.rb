c9a34437c8ef35c6e10c71360c927f2fcaafbfb3
selvadrew <andrew.selvadurai6@gmail.com>
2017-10-05 16:16:06 -0400

    updated refile cdn s3..v4

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,4 @@
 Paperclip::Attachment.default_options[:url] = 'eventphoto1.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'd1vc1zztydspts.cloudfront.net'
 
