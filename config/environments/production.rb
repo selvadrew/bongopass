7f803e7f892fdadce53224a0ec10298f9cc69168
selvadrew <andrew.selvadurai6@gmail.com>
2017-10-05 05:15:04 -0400

    got rid of cloudfront for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   config.action_controller.asset_host = "d1vc1zztydspts.cloudfront.net"
-   config.public_file_server.enabled = true
+   #config.action_controller.asset_host = "d1vc1zztydspts.cloudfront.net"
+  # config.public_file_server.enabled = true
 
 
   # Specifies the header that your server uses for sending files.
