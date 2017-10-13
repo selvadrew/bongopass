4d01650c2a79bf9752bf209df3425273a21dc0d0
selvadrew <andrew.selvadurai6@gmail.com>
2017-10-13 09:10:21 -0400

    fb share test 26

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,8 +29,7 @@ class EventsController < ApplicationController
               description: @event.event_description,
               type:     'article',
               url:      event_url(@event),
-              image:    @photos[0].image.url(:original),
-              app_id:   '1870680056482347'
+              image:    @photos[0].image.url(:original)
             }#,
             #alternate: [
             #  { href: 'http://example.fr/base/url', hreflang: 'fr' },
