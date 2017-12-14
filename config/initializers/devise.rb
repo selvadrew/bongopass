68030dc522dfaf038da263db93388d35bedf4b72
selvadrew <andrew.selvadurai6@gmail.com>
2017-12-14 06:42:58 -0500

    devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -284,6 +284,10 @@ Devise.setup do |config|
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {}
 
 
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
+
+
+
 
 end
 
