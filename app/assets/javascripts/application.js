dacff0c1e33bc3a7493d64ddb8830a1efe3f962d
selvadrew <andrew.selvadurai6@gmail.com>
2017-12-18 01:59:15 -0500

    more stylesheet dark theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -107,3 +107,32 @@ $(document).on('turbolinks:load', function() {
 
 
 
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
+       return false;
+    } // End if
+  });
+});
+
+
+
+var sidebar = document.getElementById('sidebar');
+Stickyfill.add(sidebar);
+
+
