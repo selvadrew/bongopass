ef3b8865d574263e584d5afe23c3ad76f5f37370
selvadrew <andrew.selvadurai6@gmail.com>
2018-01-10 01:02:59 -0500

    update UI 12

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
 	def send_email_to_buyer(buyer, event)
 	    @buyer = buyer
 	    @event = event
-	    mail(to: @buyer.email, subject: "Order Confirmed - Now share your link and receieve up to $100")
+	    mail(to: @buyer.email, subject: "Order Confirmed - Now share your link and receive up to $100")
   	end
 end 
