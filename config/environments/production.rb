635d5dccadab1362cab6291f0308ecfffef7e817
selvadrew <andrew.selvadurai6@gmail.com>
2018-01-17 10:40:21 -0500

    ready for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "d1vc1zztydspts.cloudfront.net"
+  config.action_controller.asset_host = "https://d1vc1zztydspts.cloudfront.net"
   config.public_file_server.enabled = true
 
 
